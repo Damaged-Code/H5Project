6295ddeb2ce31abb2fa3aaaaeb5873919dbf73f4
HeXin <sanjin233@gmail.com>
2018-03-26 19:02:14 +0800

    react my-app的改动

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,13 +1,45 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import './App.css';
 import Thermometer from './thermometer/Thermometer'
+import ToDoList from './todo-list/ToDoList'
 
+class Home extends Component { 
+    render(){ 
+        return (
+            <div>
+                <h2>Home</h2>
+                <p>Hello React</p>
+            </div>
+        )
+    }
+}
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-	      <Thermometer/>
-      </div>
+      return (
+        <Router>
+              <div className="App">
+              <ul>
+                      <li>
+                          <Link to="/">Home</Link>
+                      </li>
+                      <li>
+                          <Link to="/thermometer">Thermometer</Link>
+                      </li>
+                      <li>
+                          <Link to="/todolist">ToDoList</Link>
+                      </li>
+                      
+             </ul>
+            <Route exact path="/" component={Home}/>
+            <Route path="/thermometer" component={Thermometer} />
+            <Route path='/todolist' component={ToDoList}></Route>      
+            </div>
+        </Router>
     );
   }
 }
---
+++ React/my-app/src/todo-list/ToDoList.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react'
+import './list.css'
+
+class ToDoListRemove extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+    dataChange = (e) => {
+        let
+            tag = e.target,
+            index = tag.getAttribute('index'),
+            data = this.props.datas
+        data.splice(index, 1)
+        this.props.onDataChange(data)
+        
+     }
+    render() {
+        const data = this.props.datas
+        return (
+            <ul className="list">
+                {data.map((val, key) => {
+                    return (
+                        <li key={key}>
+                            {val}
+                            <button index={key} onClick={this.dataChange}>del</button>
+                        </li>
+                    )
+                })}
+            </ul>
+        )
+    }
+}
+
+class ToDoListInput extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            listVal: '',
+        }
+    }
+    handleChange = (e) => {
+        this.setState({
+            listVal: e.target.value
+        })
+    }
+    commitData = (e) => { 
+        let
+            input = this.refs.input,
+            data = this.props.datas
+        
+        data.push(input.value)
+        this.props.onDataChange(data)
+    }
+
+    render() {
+        return (
+            <div>
+                <input
+                    type="text"
+                    value={this.state.listVal}
+                    onChange={this.handleChange}
+                    ref='input'
+                />
+                <button onClick={this.commitData}>commit</button>
+            </div>
+        )
+    }
+}
+class ToDoListComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: []
+        }
+    }
+    handleChange = data => {
+        this.setState({
+            value: data
+        })
+    }
+    render() {
+        return (
+            <div>
+                <ToDoListInput
+                    datas={this.state.value}
+                    onDataChange={this.handleChange}
+                />
+                <ToDoListRemove datas={this.state.value} onDataChange={this.handleChange}/>
+            </div>
+        )
+    }
+}
+
+class ToDoList extends Component {
+    render() {
+        return <ToDoListComponent />
+    }
+}
+
+export default ToDoList
