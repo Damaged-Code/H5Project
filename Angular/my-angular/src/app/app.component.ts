5acf9de1a3f9ecdf14c652de42fb183889342d2f
SanJin-hx <sanjin233@gmail.com>
2018-04-12 14:19:26 +0800

    todolist

--- Angular/my-angular/src/app/app.component.ts
+++ Angular/my-angular/src/app/app.component.ts
@@ -6,9 +6,7 @@ import { Component } from '@angular/core'
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app'
+  title = 'ToDoList'
 
-  test(e): void {
-    console.log(e)
-  }
+
 }
