9f9ec605081445e9bfff3e8696e3473f0c3314c1
SanJin-hx <sanjin233@gmail.com>
2018-04-16 18:57:07 +0800

    =-=

--- React/element/src/utils/storage.js
+++ React/element/src/utils/storage.js
@@ -1,19 +1,23 @@
 export class LocalStorage {
   set(name, val) {
     if (typeof val === 'string') {
-      localStorage.setItem(name, val)
+      localStorage.setItem(name, val);
     } else {
-      localStorage.setItem(name, JSON.stringify(val))
+      localStorage.setItem(name, JSON.stringify(val));
     }
   }
   get(name) {
     if (localStorage.getItem(name)) {
-      return JSON.parse(localStorage.getItem(name))
+      return JSON.parse(localStorage.getItem(name));
     }
-    return undefined
+    return undefined;
   }
   del(name) {
-    localStorage.removeItem(name)
+    if (localStorage.getItem(name)) {
+      localStorage.removeItem(name);
+    } else {
+      return undefined;
+    }
   }
 }
 export class SessionStorage {}
