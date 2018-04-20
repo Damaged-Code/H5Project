2aea640f56db1a7705a11902bb3d15ed0bc9a94f
SanJin-hx <sanjin233@gmail.com>
2018-04-20 12:55:30 +0800

    vue router

--- YoHo/src/store/modules/getUrl.js
+++ YoHo/src/store/modules/getUrl.js
@@ -1,19 +1,19 @@
 import tabChoose from './tabChoose';
 
 const getUrl = {
-  state:{
-    index_url:'',
+  state: {
+    index_url: '',
   },
-  mutations:{
-    SET_URL:(state,index_url)=>{
-      state.index_url = index_url
-    }
+  mutations: {
+    SET_URL: (state, index_url) => {
+      state.index_url = index_url;
+    },
   },
-  actions:{
+  actions: {
     //设置当前页面
-    setUrl({commit},index_url){
-      commit('SET_URL',index_url)
-    }
-  }
-}
-export default getUrl
+    setUrl({ commit }, index_url) {
+      commit('SET_URL', index_url);
+    },
+  },
+};
+export default getUrl;
